905cd4921ce4ecd078018dd1fcf6ddcc62b71b05
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-03-10 04:31:42 +0530

    React Forms

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+
+// setting state and using state
+
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [headingText, setText] = useState("");
+
+  
+  function handleChange(event){
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+  function changeText(event){
+    setText(name);
+
+    event.preventDefault();
+  }
+
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={changeText}>
+      <input onChange={handleChange} type="text" placeholder="What's your name?" value={name} />
+      <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
