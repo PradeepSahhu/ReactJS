e68d07c9e616ee6d19df636ea9c399e3fd9605df
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-03-11 17:51:54 +0530

    Updated es-6-spread operator and changing complex states

--- es6-spread-operator-practice/src/components/App.jsx
+++ es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event){
+
+    const newData = event.target.value;
+    
+
+    setInputText(newData);
+  }
+
+  function addItem(){
+    setItems((prevItems) =>{
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText}/>
+        <button type="submit" onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => {
+            return <li> {todoItem}</li>
+          }
+          )}
         </ul>
       </div>
     </div>
